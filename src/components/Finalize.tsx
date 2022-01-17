0ab77f945b02266dd1b3bbb0fb947d75345829e8
nikhilgupta58 <nikhil@tartanhq.com>
2022-01-17 15:05:41 +0530

    final

--- src/components/Finalize.tsx
+++ src/components/Finalize.tsx
@@ -1,45 +1,82 @@
-import { HStack, Button } from '@chakra-ui/react';
+import { HStack, Button, Box, Text } from '@chakra-ui/react';
 import * as React from 'react'
 import { stepStore } from "../store";
+import Preview from './Preview';
+import Resume from './Resume';
+import { HiOutlineDocumentDownload } from 'react-icons/hi'
 
 export default function Finalize() {
     const { setStep } = stepStore();
 
     return (
         <>
-            <HStack spacing={5} mt={"50px"} justifyContent={"space-between"}>
-
-                <Button
-                    color={"blue"}
-                    fontWeight={"400"}
-                    bgColor={"white"}
-                    border={"1px solid blue"}
-                    onClick={() => {
-                        setStep(4)
-                    }}
-                    px="40px"
-                    _active={{ border: '1px solid blue' }}
-                    _focus={{ border: '1px solid blue' }}
-                    borderRadius={'0'}
+            <Box m={'15px'} width={{ lg: '50vw', md: '70vw', sm: '70vw' }}>
+                <HStack justifyContent={"space-between"}>
+                    <Box
+                        pr={"100px"}
+                    >
+                        <Text
+                            fontSize={{ lg: "25px" }}
+                            color={"#3983fa"}
+                            m={'10px 0 5px 0'}
+                        >
+                            Finalize your resume
+                        </Text>
+                    </Box>
+                </HStack>
+                <Box
+                    p="40px 10px 0 0"
                 >
-                    Back
-                </Button>
+                    <Box>
+                        <HStack spacing={5} mt={"50px"} justifyContent={'space-around'} >
+                            <Box
+                                alignSelf={'flex-start'}
+                                color={'#58585f'}
+                                fontSize={'18px'}
+                                lineHeight={'18px'}
+                                border={'2px solid #dee2e6'}
+                                marginTop={'12px'}
+                                padding={'18px'}
+                                fontWeight={'800'}
+                                bgColor={'gray.300'}
+                                cursor={'pointer'}
+                                _hover={{bgColor:'gray.400'}}
+                            >
+                                <HStack>
+                                    <HiOutlineDocumentDownload />
+                                    <Text
+                                    >Download</Text>
+                                </HStack>
+                            </Box>
+                            <Box minW={'610px'} minH={'893px'} border={'1px solid'}>
+                                <Resume />
+                            </Box>
+                        </HStack>
+                    </Box>
+                    <HStack spacing={5} mt={"50px"} >
 
-                <Button
-                    colorScheme='red'
-                    type='submit'
-                    onClick={() => {
+                        <Button
+                            color={"blue"}
+                            fontWeight={"400"}
+                            bgColor={"white"}
+                            border={"1px solid blue"}
+                            onClick={() => {
+                                setStep(4)
+                            }}
+                            px="40px"
+                            _active={{ border: '1px solid blue' }}
+                            _focus={{ border: '1px solid blue' }}
+                            borderRadius={'0'}
+                        >
+                            Back
+                        </Button>
 
-                    }}
-                    px="40px"
-                    _active={{ border: 'none' }}
-                    _focus={{ border: 'none' }}
-                    borderRadius={'0'}
-                >
-                    Next
-                </Button>
+                    </HStack>
 
-            </HStack>
+                </Box>
+            </Box>
         </>
+
+
     )
 }
--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -24,94 +24,98 @@ export default function Summary() {
     }
 
     if (section)
-        return (<Section setSection={setCheckSec} />
+        return (
+            <>
+                <Section setSection={setCheckSec} />
+            </>
         )
-    return (
-        <>
-            <Box m={'15px'} width={{ lg: '50vw', md: '70vw', sm: '70vw' }}>
-                <HStack justifyContent={"space-between"}>
-                    <Box
-                        pr={"100px"}
-                    >
-                        <Text
-                            fontSize={{ lg: "25px" }}
-                            color={"#3983fa"}
-                            m={'10px 0 5px 0'}
+    else
+        return (
+            <>
+                <Box m={'15px'} width={{ lg: '50vw', md: '70vw', sm: '70vw' }}>
+                    <HStack justifyContent={"space-between"}>
+                        <Box
+                            pr={"100px"}
                         >
-                            Briefly tell us about your background
-                        </Text>
-                    </Box>
-                    <Preview />
-                </HStack>
-                <Box
-                    p="40px 10px 0 0"
-                >
-                    <Box>
-                        <Textarea
-                            value={summary}
-                            height={"300px"}
-                            id='desc'
-                            resize={'none'}
-                            placeholder='Write your summary here'
-                            p={'20px'}
-                            onChange={(e) => setSummary(e.target.value)}
-                            sx={{
-                                '&::-webkit-scrollbar': {
-                                    width: '7px',
-                                    borderRadius: '10px',
-                                    backgroundColor: `rgba(0, 0, 0, 0.08)`,
-                                    boxShadow: '0 0 1px rgb(0 0 0 / 10%)',
-                                    padding: '1px'
-                                },
-                                '&::-webkit-scrollbar-thumb': {
-                                    backgroundColor: `rgba(0, 0, 0, 0.08)`,
-                                    padding: '1px',
-                                    boxShadow: '0 0 1px rgb(0 0 0 / 10%)',
-                                },
-                            }}
-                            _active={{ bgColor: 'white', border: 'gray.200' }}
-                            _focus={{ bgColor: 'white', border: 'gray.200' }}
-                            overflowX={'hidden'}
-                            fontSize={'12px'}
-                            borderRadius={'0px'}
-                        />
-                    </Box>
-                    <HStack spacing={5} mt={"50px"} justifyContent={"space-between"}>
-
-                        <Button
-                            color={"blue"}
-                            fontWeight={"400"}
-                            bgColor={"white"}
-                            border={"1px solid blue"}
-                            onClick={() => {
-                                setStep(3)
-                            }}
-                            px="40px"
-                            _active={{ border: '1px solid blue' }}
-                            _focus={{ border: '1px solid blue' }}
-                            borderRadius={'0'}
-                        >
-                            Back
-                        </Button>
-
-                        <Button
-                            colorScheme='red'
-                            type='submit'
-                            onClick={() => {
-                                setSection(true)
-                            }}
-                            px="40px"
-                            _active={{ border: 'none' }}
-                            _focus={{ border: 'none' }}
-                            borderRadius={'0'}
-                        >
-                            Next: Extra Sections
-                        </Button>
-
+                            <Text
+                                fontSize={{ lg: "25px" }}
+                                color={"#3983fa"}
+                                m={'10px 0 5px 0'}
+                            >
+                                Briefly tell us about your background
+                            </Text>
+                        </Box>
+                        <Preview />
                     </HStack>
+                    <Box
+                        p="40px 10px 0 0"
+                    >
+                        <Box>
+                            <Textarea
+                                value={summary}
+                                height={"300px"}
+                                id='desc'
+                                resize={'none'}
+                                placeholder='Write your summary here'
+                                p={'20px'}
+                                onChange={(e) => setSummary(e.target.value)}
+                                sx={{
+                                    '&::-webkit-scrollbar': {
+                                        width: '7px',
+                                        borderRadius: '10px',
+                                        backgroundColor: `rgba(0, 0, 0, 0.08)`,
+                                        boxShadow: '0 0 1px rgb(0 0 0 / 10%)',
+                                        padding: '1px'
+                                    },
+                                    '&::-webkit-scrollbar-thumb': {
+                                        backgroundColor: `rgba(0, 0, 0, 0.08)`,
+                                        padding: '1px',
+                                        boxShadow: '0 0 1px rgb(0 0 0 / 10%)',
+                                    },
+                                }}
+                                _active={{ bgColor: 'white', border: 'gray.200' }}
+                                _focus={{ bgColor: 'white', border: 'gray.200' }}
+                                overflowX={'hidden'}
+                                fontSize={'12px'}
+                                borderRadius={'0px'}
+                            />
+                        </Box>
+                        <HStack spacing={5} mt={"50px"} justifyContent={"space-between"}>
 
+                            <Button
+                                color={"blue"}
+                                fontWeight={"400"}
+                                bgColor={"white"}
+                                border={"1px solid blue"}
+                                onClick={() => {
+                                    setStep(3)
+                                }}
+                                px="40px"
+                                _active={{ border: '1px solid blue' }}
+                                _focus={{ border: '1px solid blue' }}
+                                borderRadius={'0'}
+                            >
+                                Back
+                            </Button>
+
+                            <Button
+                                colorScheme='red'
+                                type='submit'
+                                onClick={() => {
+                                    setSection(true)
+                                }}
+                                px="40px"
+                                _active={{ border: 'none' }}
+                                _focus={{ border: 'none' }}
+                                borderRadius={'0'}
+                            >
+                                Next: Extra Sections
+                            </Button>
+
+                        </HStack>
+
+                    </Box>
                 </Box>
-            </Box>
-        </>
-    )
+            </>
+        )
 }
